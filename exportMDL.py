79ab83589d57f985a01873a83066c2ebd3e36785
arif@XPS <arifer1995@gmail.com>
2020-09-02 00:19:08 +0800

    Fixed issue with overflow on Windows CMD and Powershell

--- exportMDL.py
+++ exportMDL.py
@@ -74,7 +74,7 @@ def soup(link, params=None, headers=None, cookies=None, data=None, JSON=False, t
 
 
 # Retrieved from @Greenstick on StackOverFlow in post /questions/3173320
-def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=80, fill='█', printEnd="\r"):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
